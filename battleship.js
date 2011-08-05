1d2edbcf7536001a597649c7d1bd1af45c27ca98
Gauge <gauger16@gmail.com>
2011-08-04 21:31:49 -0700

    fixed bugs started work on actions

--- battleship.js
+++ battleship.js
@@ -9,7 +9,21 @@ if (typeof Object.create !== 'function') {
 
 
 var Battleship = (function () {
+	
+	// LOOK UP OBJECTS
+	var Shiptype = {
+		carrier: {length: 5, status: [0,0,0,0,0]},
+		battleship: {length: 4, status: [0,0,0,0]},
+		destroyer: {length: 3, status: [0,0,0]},
+		submerine: {length: 3, status: [0,0,0]},
+		cruiser: {length: 2, status: [0,0]}
+	}
+	
+	var Ruleset = {
+		normal: {size: [10,10]}
+	}
 
+	// GAME OBJECTS
 	var Ship = {
 		type: '',
 		location: null,
@@ -32,14 +46,6 @@ var Battleship = (function () {
 			this.status[i] = 1;
 		}
 	}
-	
-	var Shiptype = {
-		carrier: {length: 5, status: [0,0,0,0,0]},
-		battleship: {length: 4, status: [0,0,0,0]},
-		destroyer: {length: 3, status: [0,0,0]},
-		submerine: {length: 3, status: [0,0,0]},
-		cruiser: {length: 2, status: [0,0]}
-	}
 
 	var Fleet = {
 		ships: []
@@ -54,23 +60,55 @@ var Battleship = (function () {
 		actions: [],
 	}
 
+
+	// GAME LOGIC
 	var Battle = {
 		players: [],
 		seas: {},
 		size: null,
 		ruleset: null,
-		add_player: function (player_id) {
+		
+		// creates a player id and sea for new player
+		join: function (player_id) {
 			this.players.push(player_id);
 			var sea = Object.create(Sea);
 			this.seas[player_id] = sea;
 		},
-		add_player_fleet: function (player_id, fleet) {
+		
+		// deploys fleets
+		deploy: function (player_id, fleet) {
 			var boardsize = game.size;
 			var vfleet = this.validate_fleet(fleet, boardsize);
 			if (this.seas.hasOwnProperty(player_id) && vfleet) {
+				// addes modified and validated fleet to the players fleet 
+				// and returns the updated fleet to client
 				this.seas[player_id].fleet = vfleet;
+				return vfleet;
 			}
+			// if validation failes sends an error back to client
+			return null;
 		},
+		
+		// action prossesor
+		enact: function (player_id, action){
+			var boardsize = game.size;
+			var vaction = this.validate_action(action, boardsize);
+			if (this.seas.hasOwnProperty(player_id) && vaction) {
+				// genrates a report to send back to client
+				var report = this.create_report(vaction);
+				return report;
+			}
+			// if validation failes sends an error back to client
+			return null;
+		},
+		
+		create_report: function(action){
+			// go to players defenders see
+			// check for hit
+			//create report object that will return hit miss or sunk exetra
+			//return the object
+		},
+		
 		validate_fleet: function (fleet, boardsize) {
 			for (i=0; i<fleet.ships.length; i++){
 				
@@ -83,11 +121,26 @@ var Battleship = (function () {
 				
 				var	size = vship.status.length;
 				var ship_end = game.get_ship_end(vship.location, vship.orientation, size);
-				if (ship_end === null){
+				
+				//TODO replace this mess with something a little nicer
+				if (ship_end === null || ship_end[0] >= boardsize[0] || ship_end[1] >= boardsize[1] 
+				    || ship_end[0] < 0 || ship_end[1] < 0 || vship.location[0] >= boardsize[0] ||
+				    vship.location[1] >= boardsize[1] || vship.location[0] < 0 || vship.location[1] < 0){
 					return null;
 				}
+				fleet.ships[i] = vship
 			}
 			return fleet;
+		},
+		
+		validate_action: function(action, boardsize){
+			if (action.type === 'shot' && game.seas.hasOwnProperty(action.id)){
+				if (action.location[0] < boardsize[0] && action.location[1] < boardsize[1] 
+				    && action.location[0] >= 0 && action.location[1] >= 0){
+						return action
+				}
+			}
+			return null;
 		},
 		
 		get_ship_end: function(loc, ori, size) {
@@ -111,6 +164,8 @@ var Battleship = (function () {
 		}
 	}
 
+
+	//TODO replace with server genrated id
 	var next_game_id = 17;
 	var games = {};
 
@@ -118,19 +173,27 @@ var Battleship = (function () {
 		return next_game_id++;
 	}
 	
+	// creates a populated game object ready to play
 	var create_game = function (players, options) {
+		
 		var game = Object.create(Battle);
-		game.size = options.size;
+		var rs = Object.create(Ruleset);
+		
 		game.ruleset = options.ruleset;
+		game.size = rs[game.ruleset].size;
+		
 		for (player in players) {
-			game.add_player(players[player]);
+			game.join(players[player]);
 		}
+		
+		//TODO replace with server genrated id
 		var game_id = get_next_game_id();
 		game.game_id = game_id;
 		games[game_id] = game;
 		return game;
 	}
 	
+	// i don't think we will need this
 	var get_game = function (game_id) {
 		return games[game_id];
 	}
@@ -143,18 +206,19 @@ var Battleship = (function () {
 	return that;
 })();
 
-// node js server example
+// NODE JS SERVER EXAMPLE
 var game = Battleship.create_game(['scott', 'ben'],
             {
                 ruleset: 'normal',
                 size: [10,10]
             })
-game.add_player_fleet('scott', {
+game.deploy('scott', {
 	ships: [{type: 'carrier', location: [0,0], orientation: 's'},
 	        {type: 'destroyer', location: [3,3], orientation: 'e'}]
 });
-game.add_player_fleet('ben', {
+game.deploy('ben', {
 	ships: [{type: 'carrier', location: [0,0], orientation: 's'},
 	        {type: 'destroyer', location: [3,3], orientation: 'e'}]
 });
 
+game.enact('scott', {id: 'ben', type: 'shot', location: [0,2]})
