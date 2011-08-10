2132ac0167370d6fa8c33921b09323e011439f93
kubex <huechelu@gmail.com>
2011-08-10 13:45:12 -0500

    created about and contact pages

--- app.js
+++ app.js
@@ -37,4 +37,12 @@ app.get('/', function (req, res) {
     res.render('index');
 });
 
+app.get('/about', function (req, res) {
+    res.render('about');
+});
+
+app.get('/contact', function (req, res) {
+    res.render('contact');
+});
 app.listen(3000);
+
