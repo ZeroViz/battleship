fc6da06211e242d456ce5a49d94d99525270098c
Scott Hamilton <mcleopold@gmail.com>
2011-08-16 22:02:43 -0700

    fixed game creation tests

--- test/game-create-test.js
+++ test/game-create-test.js
@@ -14,27 +14,26 @@ vows.describe('Creating a Game').addBatch({
            })},
 
     'we get a game class with 2 seas and the proper size board': function (topic) {
-      assert.deepEqual (topic, {
-        id: 1,
-        players: [ { id: 1 }, { id: 2 } ],
-        seas: { 1: {}, 2: {} },
-        ruleset: Battleship.Ruleset.normal
-      });
-    }/*,
+      assert.deepEqual(topic.players, [ { id: 1 }, { id: 2 } ]);
+      assert.deepEqual(topic.seas, { 1: { fleet: { ships: [] }, actions: [] },
+                                     2: { fleet: { ships: [] }, actions: [] } });
+      assert.deepEqual(topic.size, [10, 10]);
+      assert.deepEqual(topic.ruleset, Battleship.Ruleset.normal);
+      assert.equal(topic.id, 1);
+    },
     'and then creating a second game': {
       topic: function () { return Battleship.create_game({
                id: 2,
                players: [3, 4]
              })},
       'the first game\'s info should not interfere with the second\'s': function (topic) {
-        assert.deepEqual (topic, {
-          id: 2,
-          players: [3,4],
-          seas: { 3: {}, 4: {} },
-          ruleset: { type: 'normal' },
-          size: [10,10]
-        });
+        assert.deepEqual(topic.players, [ { id: 3 }, { id: 4 } ]);
+        assert.deepEqual(topic.seas, { 3: { fleet: { ships: [] }, actions: [] },
+                                       4: { fleet: { ships: [] }, actions: [] } });
+        assert.deepEqual(topic.size, [10, 10]);
+        assert.deepEqual(topic.ruleset, Battleship.Ruleset.normal);
+        assert.equal(topic.id, 2);
       }
-    }*/
+    }
   }
 }).export(module);
